33913761f97440e3268cb27d562990ad7be41c92
relu <relu.cri@gmail.com>
2017-11-24 16:35:10 +0100

    Add default configuration for SEPA client

--- config.go
+++ config.go
@@ -1,8 +1,8 @@
 package sepa
 
 import (
-	"io"
 	"encoding/json"
+	"io"
 )
 
 type PortsType struct {
@@ -17,23 +17,30 @@ type PathsType struct {
 }
 
 type Configuration struct {
-	Host       string
-	Ports      PortsType
-	Paths      PathsType
-	Extended   map[string]interface{}
+	Host     string
+	Ports    PortsType
+	Paths    PathsType
+	Extended map[string]interface{}
 }
 
 type parameters struct {
 	Parameters Configuration
 }
+
 var default_config = parameters{
-	Parameters:Configuration{
-		Ports: PortsType{Http: 8000, Ws: 9000},
-		Paths: PathsType{Query: "/query", Update: "/update", Subscribe: "/subscribe"},
+	Parameters: Configuration{
+		Ports:    PortsType{Http: 8000, Ws: 9000},
+		Paths:    PathsType{Query: "/query", Update: "/update", Subscribe: "/subscribe"},
+		Extended: make(map[string]interface{}),
 	},
 }
-func ConfigFromJson(reader io.Reader)(Configuration,error)  {
+
+func DefaultConfig() Configuration {
+	return default_config.Parameters
+}
+
+func ConfigFromJson(reader io.Reader) (Configuration, error) {
 	config := default_config
 	err := json.NewDecoder(reader).Decode(&config)
-	return config.Parameters,err
+	return config.Parameters, err
 }
