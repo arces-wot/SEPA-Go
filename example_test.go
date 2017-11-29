1a2eca422645e33e47cabd362b9014148b127a7d
relu <relu.cri@gmail.com>
2017-11-29 12:23:08 +0100

    Change update the example of client to check the output

--- example_test.go
+++ example_test.go
@@ -2,64 +2,88 @@ package sepa
 
 import (
 	"fmt"
-	"log"
-	"sync"
 	"github.com/arces-wot/SEPA-Go/sepa/sparql"
+	"log"
+	"sort"
+	"sync"
 )
 
 func Example() {
-		cli := NewDefaultClient("wot.arces.unibo.it")
+	cli := NewDefaultClient("localhost")
 
-		err := cli.Update(`INSERT DATA
+	err := cli.Update(`INSERT DATA
 	{
 		<http://example/lang> <http://example/thebest> "go"
 	}`)
 
-		err = cli.Update(`DELETE DATA
+	err = cli.Update(`DELETE DATA
 	{
 		<http://example/lang> <http://example/thebest> "go".
 		<http://example/lang> <http://example/theworst> "visual basic"
 	}`)
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		res, err := cli.Query("Select * Where { ?a ?b ?c}")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := cli.Query("Select * Where { ?a ?b ?c}")
 
-		if err != nil {
-			fmt.Println(err)
-			return
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	vars := res.Vars()
+	fmt.Println("Printing vars:")
+	for _, varable := range vars {
+		for _, term := range res.Bindings()[varable] {
+			fmt.Println(term)
 		}
-		vars := res.Vars()
-		for _, varable := range vars {
-			for _, term := range res.Bindings()[varable] {
-				fmt.Println(term)
+	}
+	fmt.Println("")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	sub, _ := cli.Subscribe("Select * Where { ?a ?b ?c}", func(notification *sparql.Notification) {
+		fmt.Println("Printing notification:")
+		for _, solution := range notification.AddedResults.Solutions() {
+
+			//Sorting keys to get a predictable output
+			// see https://blog.golang.org/go-maps-in-action#TOC_7.
+			var keys []string
+			for k := range solution {
+				keys = append(keys, k)
 			}
-		}
-		var wg sync.WaitGroup
-		wg.Add(1)
-		sub, _ := cli.Subscribe("Select * Where { ?a ?b ?c}", func(notification *sparql.Notification) {
+			sort.Strings(keys)
 
-			for _, solution := range notification.AddedResults.Solutions() {
-				for key, term := range solution {
-					fmt.Print(key,": ",term," ")
-				}
-				fmt.Println()
+			for _, key := range keys {
+				fmt.Print(key, ": ", solution[key], " ")
 			}
+			fmt.Println(".")
+		}
 
-			wg.Done()
-		})
-		fmt.Println(sub)
-		err = cli.Update(`INSERT DATA
+		wg.Done()
+	})
+
+	err = cli.Update(`INSERT DATA
 	{
 		<http://example/lang> <http://example/thebest> "go".
 		<http://example/lang> <http://example/theworst> "visual basic"
 	}`)
 
-		log.Println(err)
-		wg.Wait()
-		sub.Unsubscribe()
-		fmt.Println("Unsubscribe")
-		// Output: Unsubscribe
+	log.Println(err)
+	wg.Wait()
+	sub.Unsubscribe()
+	fmt.Println("")
+	fmt.Println("Unsubscribed")
+
+	// Output:
+	//Printing vars:
+	//http://example/lang
+	//http://example/thebest
+	//Go
+	//
+	//Printing notification:
+	//a: http://example/lang b: http://example/thebest c: go .
+	//a: http://example/lang b: http://example/theworst c: visual basic .
+	//
+	//Unsubscribed
 }
